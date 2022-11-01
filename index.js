b86e951c4ed4cd215604079c8131c8417ee36994
Kyle Ivan Eduvane <hgfrsh@gmail.com>
2022-11-01 16:43:44 +0800

    top up design

--- index.js
+++ index.js
@@ -292,7 +292,7 @@ app.get("/items2/view/:id", (req,res) =>{
                     else if(!error){
                         console.log('good item query 2')
                         console.log(result)
-                        res.render('pages/view-item', { result, user:req.session.user, result_date:dates.rows, cart_count:req.session.cart_count })
+                        res.render('pages/view-item', { result, user:req.session.user, result_date:dates.rows[0], cart_count:req.session.cart_count })
                     }
                 })
 
