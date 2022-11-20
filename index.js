93213968de357bb97cd2a8181b6e5609c82a99df
Kyle Ivan Eduvane <hgfrsh@gmail.com>
2022-11-20 17:02:45 +0800

    emailform changes

--- index.js
+++ index.js
@@ -406,7 +406,40 @@ app.post("/emailform", (req,res) => {
           to: `${email}`, // list of receivers
           subject: "EMAIL OTP - Mang-Hiram", // Subject line
           text: "Hello Nae", // plain text body
-          html: `<b>OTP: ${req.session.emailotp}</b><br>`, // html body
+          html: `<body style="font-family: Helvetica, Arial, sans-serif; margin: 0px; padding: 0px; background-color: #ffffff;">
+          <table role="presentation"
+            style="width: 100%; border-collapse: collapse; border: 0px; border-spacing: 0px; font-family: Arial, Helvetica, sans-serif; background-color: rgb(239, 239, 239);">
+            <tbody>
+              <tr>
+                <td align="center" style="padding: 1rem 2rem; vertical-align: top; width: 100%;">
+                  <table role="presentation" style="max-width: 600px; border-collapse: collapse; border: 0px; border-spacing: 0px; text-align: left;">
+                    <tbody>
+                      <tr>
+                        <td style="padding: 40px 0px 0px;">
+                          <div style="text-align: left;">
+                            <div style="padding-bottom: 20px;"><img src="https://i.ibb.co/Qbnj4mz/logo.png" alt="Company" style="width: 56px;"></div>
+                          </div>
+                          <div style="padding: 20px; background-color: rgb(255, 255, 255);">
+                            <div style="color: rgb(0, 0, 0); text-align: left;">
+                              <h1 style="margin: 1rem 0">Verification code</h1>
+                              <p style="padding-bottom: 16px">Please use the verification code below to sign in.</p>
+                              <p style="padding-bottom: 16px"><strong style="font-size: 130%">${req.session.emailotp}</strong></p>
+                              <p style="padding-bottom: 16px">If you didn’t request this, you can ignore this email.</p>
+                              <p style="padding-bottom: 16px">Thanks,<br>The Orbit team</p>
+                            </div>
+                          </div>
+                          <div style="padding-top: 20px; color: rgb(153, 153, 153); text-align: center;">
+                            <p style="padding-bottom: 16px">Made with ♥ in Paris</p>
+                          </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </body>`, // html body
         });
       
         console.log("Message sent: %s", info.messageId);
