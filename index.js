6133a945770f618121550e8f50039b0692351c4d
Kyle Ivan Eduvane <hgfrsh@gmail.com>
2022-11-21 16:37:41 +0800

    changes

--- index.js
+++ index.js
@@ -524,7 +524,7 @@ app.get("/dashboard/seller/requests/denied", (req,res) => {
 })
 
 app.get("/item/listing", (req,res) => {
-    res.render('pages/item_list')
+    res.render('pages/item_list', { user:req.session.username, cart_count: req.session.cart_count, cuurency:req.session.currency })
 })
 
 //logout
