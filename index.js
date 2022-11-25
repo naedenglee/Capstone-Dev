21b76474f7c3b7a40f147a8c50cdaffebac323f6
Kyle Ivan Eduvane <hgfrsh@gmail.com>
2022-11-25 14:25:18 +0800

    image upload

--- index.js
+++ index.js
@@ -644,29 +644,30 @@ app.get("/item/listing", (req,res) => {
     
 })
 
-app.post("/item/listing", upload.single("imageFile"), async(req,res) => {   
+app.post("/item/listing", async(req,res) => {   
     //cloudinary upload function 
     const uploadImage = async (imagePath) => {
 
         // Use the uploaded file's name as the asset's public ID and 
         // allow overwriting the asset with new versions
         const options = {
-          use_filename: true,
-          unique_filename: false,
-          overwrite: true,
+            use_filename: true,
+            unique_filename: false,
+            overwrite: true,
+            folder:'mang-hiram-seller-pictures'
         };
     
         try {
-          // Upload the image
-          const result = await cloudinary.uploader.upload(imagePath, options);
-          console.log(result);
-          return result.secure_url;
+            // Upload the image
+            const result = await cloudinary.uploader.upload(imagePath, options);
+            console.log(result);
+            return result.secure_url;
         } catch (error) {
-          console.error(error);
+            console.error(error);
         }
     };
 
-    const imagePath = req.file.path;
+    const imagePath = req.body.imageFileb64;
 
     // Upload the image
     const imageUrl = await uploadImage(imagePath);
