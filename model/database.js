cf1b56abeff4d71ac2933d5585545b1c463478b0
Loo <loo@pop-os.localdomain>
2022-11-28 14:47:58 +0800

    change db

--- model/database.js
+++ model/database.js
@@ -1,31 +1,15 @@
 
 //for postgres
 //const { Client } = require('pg')
-const { Client, Pool } = require('pg')
+const {Pool } = require('pg')
 
-//const client = new Client({
-//    connectionString: process.env.DATABASE_URL,
-//    ssl: {
-//      rejectUnauthorized: false
-//    }
-//  });
-//
-const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'root',
-    port: 5432,
+const pool = new pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
 
-});
-
-pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'root',
-    port: 5432,
-})
 
 
 module.exports = {
