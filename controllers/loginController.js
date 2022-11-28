c43342dc97db80da87854794a5e92fef16b444b6
naedenglee <naedenglee.capstone@gmail.com>
2022-11-29 01:23:34 +0800

    test finally function

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -59,8 +59,6 @@ var login = async (req, res, next) =>{
                         currency = req.session.currency
                         req.session.currency = result3.rows[0].user_currency
                         req.session.user_id = vid
-                        res.redirect('/')
-                        next()
             }
         }
     }
@@ -69,6 +67,8 @@ var login = async (req, res, next) =>{
     }
     finally{
         pool.release
+        res.redirect('/')
+        next()
     }
 }
 
