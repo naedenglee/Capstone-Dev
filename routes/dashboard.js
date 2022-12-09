a623f55cd74b688b13d2f919229aa505c8af826f
Kyle Ivan Eduvane <hgfrsh@gmail.com>
2022-12-09 21:50:33 +0800

    added functions

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router() 
 const dashboardController = require('../controllers/dashboardController.js')
+const bodyParser = require('body-parser')
+router.use(bodyParser.urlencoded({extended: true, limit: '50mb'})); 
+router.use(bodyParser.json());
 
 router.get('/', dashboardController.viewMainDashboard)
 
--- routes/item.js
+++ routes/item.js
@@ -2,8 +2,7 @@ const express = require('express')
 const itemController = require('../controllers/itemController.js')
 const router = express.Router() 
 const bodyParser = require('body-parser')
-router.use(bodyParser.urlencoded({extended: true}));
-
+router.use(bodyParser.urlencoded({extended: true, limit: '50mb'})); 
 router.use(bodyParser.json());
 //availability(reservation calendar)
 router.get("/", itemController.allItemView) //All Item Page
