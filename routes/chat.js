331402c4ed9948d6dd6154d31911a6c70fbeb237
naedenglee <naedenglee.capstone@gmail.com>
2023-02-01 23:19:08 +0800

    chat bugs fixed

--- routes/chat.js
+++ routes/chat.js
@@ -5,9 +5,14 @@ const bodyParser = require('body-parser')
 router.use(bodyParser.urlencoded({extended: true, limit: '50mb'})); 
 router.use(bodyParser.json());
 //availability(reservation calendar)
-router.get("/subscribe", chatController.subs) //
-router.get("/show/:chat_id/:room_id", chatController.showMessages) 
-router.post("/show/:chat_id/:room_id", chatController.sendChat) 
+//router.get("/subscribe", chatController.subs) //
+router.get("/show/", chatController.showRoom) 
+router.get("/show/:room_id", chatController.showRoom) 
+router.get("/show/:chat_id/:room_id", chatController.showRoom)
+router.post("/show/:chat_id/:room_id", chatController.chatRoom)
+
+//router.get("/show/:chat_id/:room_id", chatController.showMessages) 
+//router.post("/show/:chat_id/:room_id", chatController.sendChat) 
 
 module.exports = router
 
